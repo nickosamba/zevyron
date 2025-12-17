ef777c4dd1b84249ee2bda46fc956d5931f0c91c
nickosamba <nickosamba.nh21@gmail.com>
2025-12-17 06:16:58 +0100

    fake

--- zevyron_projet/settings.py
+++ zevyron_projet/settings.py
@@ -15,7 +15,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+#ALLOWED_HOSTS = ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
+
+ALLOWED_HOSTS = []
 
 # Application definition
 

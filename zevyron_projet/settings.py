56ad96f277551f6d43864ca314f39dda8c5f4417
nickosamba <nickosamba.nh21@gmail.com>
2025-12-17 07:42:29 +0100

    fakes

--- zevyron_projet/settings.py
+++ zevyron_projet/settings.py
@@ -116,6 +116,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 

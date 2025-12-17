e349ec4f06c844fd27da5e8ac585fe3d15b4b826
nickosamba <nickosamba.nh21@gmail.com>
2025-12-17 01:31:37 +0100

    static_root

--- zevyron_projet/settings.py
+++ zevyron_projet/settings.py
@@ -111,7 +111,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
 # MEDIA_ROOT = BASE_DIR / 'media'
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

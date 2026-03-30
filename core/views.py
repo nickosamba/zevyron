7908c276b987b934b3e85949ea0d2dfdbfe4af8d
nickosamba <nickosamba.nh21@gmail.com>
2026-03-30 13:32:55 +0100

    ajout des balise google analytics

--- core/views.py
+++ core/views.py
@@ -7,7 +7,7 @@ from django.views.decorators.cache import cache_page
 
 
 # Create your views here.
-@cache_page(60 * 15)  # 15 minutes
+# @cache_page(60 * 15)  # 15 minutes
 def home(request):
     return render(request, 'core/index.html')
 
